4873d9cc53c1c50449b96e1834d7c84fe758237e
Sadık Uluçay <sadikulu45@gmail.com>
2024-07-10 13:45:52 +0300

    V3

--- src/pages/common/loading-page.js
+++ src/pages/common/loading-page.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
-import logo from "../../assets/img/logo/logo3.png";
+//import logo from "../../assets/img/logo/logo3.png";
 import "./loading-page.scss";
 
 const LoadingPage = () => {
   return (
     <div className="loading-page">
       <Spinner animation="border" variant="primary" />
-      <img src={logo} alt="Loading..." />
+      {/* <img 
+      src={logo} 
+      alt="Loading..." /> */}
     </div>
   );
 };
